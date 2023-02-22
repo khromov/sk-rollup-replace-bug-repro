eaf6ee55779d9443d3f9477a219b25187d1329ca
Stanislav Khromov <stanislav.khromov@schibsted.com>
2023-02-22 14:50:30 +0100

    Add reproduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+  @font-face {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    src: url('ENV_PUBLIC_CDN_URL/fonts/Lato-Regular.ttf');
+  }
+
+  h1 {
+    font-family: 'Lato'; 
+  }
+
+  @media (min-width: ENV_PUBLIC_BREAKPOINT_MOBILE) {
+    h1 {
+      font-size: 14px;
+    }
+  }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import replace from "@rollup/plugin-replace";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    replace({
+      preventAssignment: true,
+      values: {
+		ENV_PUBLIC_BREAKPOINT_MOBILE: '375px',
+        ENV_PUBLIC_CDN_URL: 'https://cdn.yoursite.com'
+      },
+    }),
+    sveltekit(),
+  ],
 });
